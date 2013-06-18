998e06596ca48a9c9677c9c77d0834a0adb66653
matuobasyouca <matuobasyouca@gmail.com>
2013-06-18 16:53:10 +0800

    result save in different table

--- src/main/java/com/zhangwoo/spider/server/dao/ConversationDao.java
+++ src/main/java/com/zhangwoo/spider/server/dao/ConversationDao.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import com.meiya.common.simpledao.BaseDao;
+import com.meiya.common.simpledao.Config;
+import com.meiya.common.string.StringUtil;
 import com.zhangwoo.spider.po.Conversation;
 
 public class ConversationDao {
@@ -16,16 +18,45 @@ public class ConversationDao {
 	public void save(List<Conversation> result) {
 		List<Object[]> parasValues = new ArrayList<Object[]>();
 		for (Conversation c : result) {
-			parasValues.add(new Object[] { c.getTitle(), c.getPublishTime(),c.getUpdateTime(),
-					c.getContent(), c.getSelfLink(), c.getAuthor(),c.getIsTopic(), c.getTid()});
+			parasValues.add(new Object[] { c.getTitle(), c.getPublishTime(),
+					c.getUpdateTime(), c.getContent(), c.getSelfLink(),
+					c.getAuthor(), c.getIsTopic(), c.getTid() });
 		}
+
+		String tableName = "conversation";
+		if (Config.getProps("saveOneTable").equals("0")) {
+			try {
+				tableName = StringUtil.match(result.get(0).getSelfLink(),
+						"http://([^/]*)")[1];
+			} catch (Exception e) {
+				logger.error("analyse table name error", e);
+			}
+		}
+
 		try {
 			dao.save(
-					"insert conversation (title,publishTime,updateTime,content,selflink,author,isTopic,tid) values (?,?,?,?,?,?,?,?)",
+					"insert `"
+							+ tableName
+							+ "` (title,publishTime,updateTime,content,selflink,author,isTopic,tid) values (?,?,?,?,?,?,?,?)",
 					parasValues);
-		} catch (SQLException e) {
-			if(e.getMessage().indexOf("Duplicate entry")==-1)
-				logger.error("",e);
+		} catch (Exception e) {
+			if (e.getMessage().indexOf("Duplicate entry") == -1)
+				logger.error("", e);
+
+			if (e.getMessage().matches("Table.*?doesn't exist")) {
+				try {
+					dao.save("CREATE TABLE `" + tableName
+							+ "` SELECT * FROM conversation WHERE 1=2");
+					dao.save(
+							"insert `"
+									+ tableName
+									+ "` (title,publishTime,updateTime,content,selflink,author,isTopic,tid) values (?,?,?,?,?,?,?,?)",
+							parasValues);
+				} catch (SQLException e1) {
+					logger.error("error happen create table `" + tableName
+							+ "` and save data!", e1);
+				}
+			}
 		}
 	}
 
